8f6502e988776d5f85f3ccd3ef9daa0a44a02f9c
Sherlock <9hoshi2004@gmail.com>
2025-12-09 12:08:39 +0700

    Added LAB3 and LAB_4
    - Added .gitignore
    - Connected Database
    - Added Docker yml
    - Added Docker File

---
+++ LAB_4/web/index.php
@@ -0,0 +1,19 @@
+<html>
+    <body>
+        <div>Hello PHP</div>
+    </body>
+    
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"></script>
+
+    <script>
+        document.getElementById("hello").innerHTML = "Hello from JavaScript!";
+
+        $(function(){});
+
+        $(document).ready(function(){
+            $("#hello").html("I LOVE FEMBOY");
+        });
+        
+    </script>
+
+</html>
---
+++ LAB_4/web/page/blog.php
@@ -0,0 +1,37 @@
+<div>My PHP</div>
+
+<?php
+echo "Hello PHP!";
+
+$name = "HOSHI";
+$age = 21;
+
+var_dump($name);
+?>
+
+<?= $name." : ".$age ?>
+
+
+<div>
+    <form action="">
+        <input type="text" name = "blog">
+        <button type="submit">SAVE</button>
+    </form>
+</div>
+
+<div>
+    <table>
+        <thead>
+            <tr>
+                <td>ID</td>
+                <td>Text</td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>ID</td>
+                <td>Text</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
